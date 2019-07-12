b4627a4e44a593b95c4c7a9018cdb92f4a4bf4c4
Akshay930 <52812359+Akshay930@users.noreply.github.com>
2019-07-12 11:41:20 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,8 @@
-num = int(input())
-if num >= 0:
-   if num == 0:
-       print("Zero")
-   else:
-       print("Positive number")
+a = int(input())
+if a >= 0:
+	if a > 0:
+		print("It  is positive")
+	else:
+		print("It is zero")
 else:
-   print("Negative number")
+	print("It is negative")
